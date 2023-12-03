44a369a3125d3a0a60c6fb9751c77ce23a84d428
thomastepi <tepexminem2013@gmail.com>
2023-12-03 11:49:39 -0500

    update book 100%

--- src/components/UpdateBook.js
+++ src/components/UpdateBook.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DefaultLayout from "./DefaultLayout";
 import books from "../assets/books";
 import { Link } from "react-router-dom";
@@ -8,7 +8,55 @@ const UpdateBook = () => {
   const [bookNumber, setBookNumber] = useState("");
   const [inputValue, setInputValue] = useState("");
   const [bookToUpdate, setBookToUpdate] = useState({});
-  
+  const [newTitle, setNewTitle] = useState("");
+  const [newAuthor, setNewAuthor] = useState("");
+  const [newPrice, setNewPrice] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //let bookNumberSelected = bookNumber - 1;
+    if (newTitle !== "") {
+      setBookList((prevBookList) => {
+        const updatedBookList = [...prevBookList];
+        updatedBookList[bookNumber - 1].title = newTitle;
+        return updatedBookList;
+      });
+      let id = bookNumber - 1;
+      for (let i = 0; i < books.length; i++) {
+        if (i === id) {
+          books[i].title = bookList[i].title;
+        }
+      }
+      console.log(books);
+    } else if (newAuthor !== "") {
+      setBookList((prevBookList) => {
+        const updatedBookList = [...prevBookList];
+        updatedBookList[bookNumber - 1].author = newAuthor;
+        return updatedBookList;
+      });
+      let id = bookNumber - 1;
+      for (let i = 0; i < books.length; i++) {
+        if (i === id) {
+          books[i].author = bookList[i].author;
+        }
+      }
+    } else if (newPrice !== "") {
+      setBookList((prevBookList) => {
+        const updatedBookList = [...prevBookList];
+        updatedBookList[bookNumber - 1].price = newPrice;
+        return updatedBookList;
+      });
+      let id = bookNumber - 1;
+      for (let i = 0; i < books.length; i++) {
+        if (i === id) {
+          books[i].price = bookList[i].price;
+        }
+      }
+    } else {
+      console.log("Please enter a valid value");
+    }
+  };
+
   const handleClick = (e) => {
     e.preventDefault();
     console.log("clicked");
@@ -17,87 +65,15 @@ const UpdateBook = () => {
     setBookToUpdate(bookList[bookNumber - 1]);
     console.log(bookToUpdate);
   };
-  
-  console.log(bookToUpdate);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-  // Update the bookToUpdate state
-  setBookToUpdate((prevBookToUpdate) => ({
-    ...prevBookToUpdate,
-    [name]: value,
-  }));
-  };
-
-  function updateTitle(e) {
-    e.preventDefault();
-    setBookToUpdate((prevBookToUpdate) => ({
-      ...prevBookToUpdate,
-      title: bookToUpdate.title,
-    }));
-    // update bookList array with updated book
-    setBookList((prevBookList) => {
-      const updatedBookList = [...prevBookList];
-      updatedBookList[bookNumber - 1] = bookToUpdate;
-      return updatedBookList;
-    });
-    let id = bookNumber - 1;
-    for (let i = 0; i < books.length; i++) {
-      if (i === id) {
-        books[i].title = bookToUpdate.title;
-      }
-    }
-    // [books] = [...books, bookList];
-    console.log("updated book list")
-    console.log(bookList);
-  }
-  
-  function updateAuthor(e) {
-    e.preventDefault();
-    setBookToUpdate((prevBookToUpdate) => ({
-      ...prevBookToUpdate,
-      author: bookToUpdate.author,
-    }));
-    // update bookList array with updated book
-    setBookList((prevBookList) => {
-      const updatedBookList = [...prevBookList];
-      updatedBookList[bookNumber - 1] = bookToUpdate;
-      return updatedBookList;
-    });
-    let id = bookNumber - 1;
-    for (let i = 0; i < books.length; i++) {
-      if (i === id) {
-        books[i].author = bookToUpdate.author;
-      }
-    }
-  }
-
-  function updatePrice(e) {
-    e.preventDefault();
-    setBookToUpdate((prevBookToUpdate) => ({
-      ...prevBookToUpdate,
-      price: bookToUpdate.price,
-    }));
-    // update bookList array with updated book
-    setBookList((prevBookList) => {
-      const updatedBookList = [...prevBookList];
-      updatedBookList[bookNumber - 1] = bookToUpdate;
-      return updatedBookList;
-    });
-    let id = bookNumber - 1;
-    for (let i = 0; i < books.length; i++) {
-      if (i === id) {
-        books[i].price = bookToUpdate.price;
-      }
-    }
-  }
-  // reset input fields
-  ;
-
-  
-  
-
+  // reset input values after submit
+  useEffect(() => {
+    setNewTitle("");
+    setNewAuthor("");
+    setNewPrice("");
+    setInputValue("");
+    setBookNumber("");
+  }, [bookList]);
   return (
     <div className="update-book">
       <DefaultLayout>
@@ -111,88 +87,87 @@ const UpdateBook = () => {
             id="book-number"
             value={inputValue}
           />
-          <button onClick={handleClick} type="button" className="btn">
+          <button onClick={handleClick} type="submit" className="btn">
             Submit
           </button>
           {bookNumber <= bookList.length && bookNumber > 0 ? (
-            <h3>
-              You have selected <em> {bookList[bookNumber - 1].title} </em> by
-              <em> {bookList[bookNumber - 1].author} </em>{" "}
-            </h3>
+            <>
+              <h3>
+                You have selected <em> {bookList[bookNumber - 1].title} </em> by
+                <em> {bookList[bookNumber - 1].author} </em>{" "}
+              </h3>
+              <form className="add-book-form" onSubmit={handleSubmit}>
+                <div>
+                  <input
+                    value={newTitle}
+                    onChange={(e) => setNewTitle(e.target.value)}
+                    placeholder="Enter new title"
+                    type="text"
+                    name="title"
+                    id="title"
+                  />
+                </div>
+                <div>
+                  <input
+                    value={newAuthor}
+                    onChange={(e) => setNewAuthor(e.target.value)}
+                    placeholder="Enter new author"
+                    type="text"
+                    name="author"
+                    id="author"
+                  />
+                </div>
+                <div>
+                  <input
+                    value={newPrice}
+                    onChange={(e) => setNewPrice(e.target.value)}
+                    placeholder="Enter new price"
+                    type="text"
+                    name="price"
+                    id="price"
+                  />
+                </div>
+                <button type="submit" className="btn">
+                  Update Book
+                </button>
+              </form>
+            </>
           ) : (
             ""
           )}
           {(bookNumber > bookList.length && bookNumber) ||
           (bookNumber <= 0 && bookNumber) ? (
             <h3>
-              There is no book associated with that number in the inventory the book number is {bookNumber}
+              Please enter a valid book number between 1 and {bookList.length}
             </h3>
           ) : (
             ""
           )}
         </div>
-        <form className="add-book-form" >
-          <div>
-            {/* <label htmlFor="title">Title</label> */}
-            <input
-              onChange={handleChange}
-              placeholder="Enter new title"
-              type="text"
-              name="title"
-              id="title"
-            />
-            <button onClick={updateTitle} className="btn">Update Title</button>
-          </div>
-          <div>
-            {/* <label htmlFor="author">Author</label> */}
-            <input
-              onChange={handleChange}
-              placeholder="Enter new author"
-              type="text"
-              name="author"
-              id="author"
-            />
-            <button onClick={updateAuthor} className="btn">Update Author</button>
-          </div>
-          <div>
-            {/* <label htmlFor="price">Price</label> */}
-            <input
-              onChange={handleChange}
-              placeholder="Enter new price"
-              type="text"
-              name="price"
-              id="price"
-            />
-            <button onClick={updatePrice} className="btn">Update Price</button>
-          </div>
-          {/* <button className="btn" type="submit">
-            Update Book
-          </button> */}
 
-          <div>
-            <h2>Current Inventory</h2>
-            <table style={{ margin: "0 auto", border: "1px solid black" }}>
-              <thead>
-                <tr>
-                  <th>Number</th>
-                  <th>Title</th>
-                  <th>Author</th>
-                  <th>Price</th>
+        <div>
+          <h2>Current Inventory</h2>
+          <table style={{ margin: "0 auto", border: "1px solid black" }}>
+            <thead>
+              <tr>
+                <th>Number</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bookList.map((book, index) => (
+                <tr key={index}>
+                  <td style={{ border: "1px solid black" }}>{index + 1}</td>
+                  <td style={{ border: "1px solid black" }}>{book.title}</td>
+                  <td style={{ border: "1px solid black" }}>{book.author}</td>
+                  <td style={{ border: "1px solid black" }}>${book.price}</td>
                 </tr>
-              </thead>
-              <tbody>
-                {bookList.map((book, index) => (
-                  <tr key={index}>
-                    <td style={{ border: "1px solid black" }}>{index + 1}</td>
-                    <td style={{ border: "1px solid black" }}>{book.title}</td>
-                    <td style={{ border: "1px solid black" }}>{book.author}</td>
-                    <td style={{ border: "1px solid black" }}>${book.price}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </form>
+              ))}
+            </tbody>
+          </table>
+        </div>
         <Link to="/main-menu" className="btn">
           Main Menu
         </Link>
